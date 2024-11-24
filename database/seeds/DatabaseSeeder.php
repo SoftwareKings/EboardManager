9cbaafb2c1ee5cce9491a843cad90a2fef2dd54a
william-martin0204 <william.martin0204@gmail.com>
2024-11-24 17:50:00 +0900

    feat: login and registration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            ['name' => 'William Martin', 'email' => 'supervenus0204@gmail.com', 'password' => 'asd']
+        ]);
     }
 }
