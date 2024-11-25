eb7c02b20e87dc7bac840fd675730cb6d5fc6c36
william-martin0204 <william.martin0204@gmail.com>
2024-11-26 07:11:20 +0900

    add user create functionality

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 // Dashboard
 Route::get('/dashboard', 'DashboardController@index')->middleware('auth')->name('dashboard');
 
-Route::middleware(['auth', 'admin'])->group(function() {
+Route::middleware([])->group(function() {
     Route::get('/admin/users', 'Admin\UserController@index')->name('admin.users.index');
+    // Route::get('/admin/users/create', 'Admin\UserController@create')->name('admin.users.create');
+    Route::post('/admin/users', 'Admin\UserController@store')->name('admin.users.store');
+    // Route::get('/admin/users/{user}/edit', 'Admin\UserController@edit')->name('admin.users.edit');
+    // Route::put('/admin/users/{user}', 'Admin\UserController@update')->name('admin.users.update');
+    // Route::delete('/admin/users/{user}', 'Admin\UserController@destroy')->name('admin.users.destroy');
 });
